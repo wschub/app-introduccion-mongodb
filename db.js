3001aa845fa4f31db3744d5cd132e4ba2c4b0e1e
dracgeek <49764404+dracgeek@users.noreply.github.com>
2019-09-09 00:16:28 -0500

    cambio conexion db

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/clasegeek', {
+//mongodb://localhost/clasegeek
+mongoose.connect('mongodb+srv://dracgeek:TURNruY3yEHTa7ng@cluster0-l1vgh.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
 }) 
     .then(db => console.log(`DB is connected`))
     .catch(err => console.error(err));
+
+    
